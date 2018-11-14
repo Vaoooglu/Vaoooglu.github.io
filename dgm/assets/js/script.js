58f35391c50ce118aec4cc87b64407e6abcf49ad
Glushakov Vladimir <vaoooglu.dn@gmail.com>
2018-11-14 22:50:56 +0300

    1411v1

--- dgm/assets/js/script.js
+++ dgm/assets/js/script.js
@@ -1,40 +1,69 @@
 $(document).ready(function() {
-	var containerMenu=$("#MainMenu");
-$("#MenuIcon").click(function(){
-	containerMenu.css("left","0px");
-	function showMenu(){
-		containerMenu.css("clip-path","polygon(0 0,100% 0, 100% 100%,0% 100%)");
-		containerMenu.css("-webkit-clip-path","polygon(0 0,100% 0, 100% 100%,0% 100%)");
-		$("#MenuIcon").animate({right: '-100'},300);
-	}
-	setTimeout(showMenu,100);
+		var containerMenu=$("#MainMenu");
+		$("#MenuIcon, #leftborder").click(function(){
+				containerMenu.css("left","0px");
+					function showMenu(){
+						containerMenu.css("clip-path","polygon(0 0,100% 0, 100% 100%,0% 100%)");
+						containerMenu.css("-webkit-clip-path","polygon(0 0,100% 0, 100% 100%,0% 100%)");
+						$("#MenuIcon").animate({right: '-100'},300);
+					}
+				setTimeout(showMenu,100);
+		});
+		$("#close").click(function(){
+			containerMenu.css("clip-path","polygon(0 0,0 0, 100% 100%,0% 100%)");
+			containerMenu.css("-webkit-clip-path","polygon(0 0,0 0, 100% 100%,0% 100%)");
+				function hideMenu(){
+					containerMenu.css("left","-350px");
+					$("#MenuIcon").animate({right: '50'},300);
+				}
+				setTimeout(hideMenu,300);
+				function originalLayout(){
+					containerMenu.css("clip-path","polygon(0 0,100% 0,0 100%,0% 100%)");
+					containerMenu.css("-webkit-clip-path","polygon(0 0,100% 0,0 100%,0% 100%)");
+				}
+				setTimeout(originalLayout,300);
+		});
 
-});
-$("#close").click(function(){
-		containerMenu.css("clip-path","polygon(0 0,0 0, 100% 100%,0% 100%)");
-		containerMenu.css("-webkit-clip-path","polygon(0 0,0 0, 100% 100%,0% 100%)");
-		function hideMenu(){
-			containerMenu.css("left","-350px");
-			$("#MenuIcon").animate({right: '50'},300);
+		$(document).mouseup(function (e){
+		var closeMenuElem = $("#MenuIcon,#leftborder, #MainMenu");
+		if (!closeMenuElem.is(e.target) && closeMenuElem.has(e.target).length === 0) {
+					closeMenu(this);
 		}
-		setTimeout(hideMenu,300);
-		function originalLayout(){
-			containerMenu.css("clip-path","polygon(0 0,100% 0,0 100%,0% 100%)");
-			containerMenu.css("-webkit-clip-path","polygon(0 0,100% 0,0 100%,0% 100%)");
-		}
-		setTimeout(originalLayout,300);
+		function closeMenu (){
+			containerMenu.css("clip-path","polygon(0 0,0 0, 100% 100%,0% 100%)");
+			containerMenu.css("-webkit-clip-path","polygon(0 0,0 0, 100% 100%,0% 100%)");
+				function hideMenu(){
+					containerMenu.css("left","-350px");
+					$("#MenuIcon").animate({right: '50'},300);
+				}
+				setTimeout(hideMenu,300);
+				function originalLayout(){
+					containerMenu.css("clip-path","polygon(0 0,100% 0,0 100%,0% 100%)");
+					containerMenu.css("-webkit-clip-path","polygon(0 0,100% 0,0 100%,0% 100%)");
+				}
+				setTimeout(originalLayout,300);
+		};
 });
 
-$(document).ready(function(){
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 450) {
-            $('#MenuIcon').fadeOut();
-        } else {
-            $('#MenuIcon').fadeIn();
-        }
-    });
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 450) {
+					$('#MenuIcon').fadeOut("fast");
+			} else {
+					$('#MenuIcon').fadeIn("fast");
+			}
+		});
+
+
+		$('.scrol-btn').click(function(){
+			$('html,body').animate({scrollTop: 0}, 1000);
+		});
 });
+function supportsImports() {
+  return 'import' in document.createElement('link');
+}
 
-
-
-});
+if (supportsImports()) {
+  console.log("Браузер поддерживает import HTML");
+} else {
+  console.log("НЕТ ПОДДЕРЖКИ import HTML");
+}
