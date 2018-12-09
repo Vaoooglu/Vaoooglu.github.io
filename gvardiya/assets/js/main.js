1c25ff8a5abf376e15da3a54c50d571152c9f7c9
Glushakov Vladimir <vaoooglu.dn@gmail.com>
2018-12-09 15:31:25 +0300

    gvardiya0912v1

--- gvardiya/assets/js/main.js
+++ gvardiya/assets/js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 								$('head').append('<link rel="stylesheet" type="text/css" href="dist/js/jquery.fullPage/jquery.fullPage.css" />');	//desktop
 								$('#page').fullpage({
 									menu: '#topnav',
-									anchors: ['homepage', 'ekonomiya', 'services', 'garant', 'advantages', 'reviews', 'how-it-work', 'sales', 'contactPage'],
+									anchors: ['homepage', 'ekonomiya', 'services', 'garant', 'advantages', 'reviews','answer-quetion', 'how-it-work', 'sales', 'contactPage'],
 									css3: true,
 									verticalCentered: false,
 									navigation: true,
@@ -19,7 +19,7 @@ $(document).ready(function() {
 									navigationPosition: 'right',
 									showActiveTooltip: true,
 									responsive: 980,
-									navigationTooltips: ['Главная', 'Экономия', 'Услуги', 'Гарантии', 'Преимущества', 'Отзывы', 'Как мы работаем', 'Акция', 'Контакты'],
+									navigationTooltips: ['Главная', 'Экономия', 'Услуги', 'Гарантии', 'Преимущества', 'Отзывы', 'Вопрос-ответ', 'Как мы работаем', 'Акция', 'Контакты'],
 									onLeave: function(index , nextIndex , direction){
 												change_slide (nextIndex);
 									}
@@ -79,6 +79,11 @@ $('.btn-popup').click(function() {
 		$('.popup-wrapper-consultation').addClass("shown")
 	}
 });
+$(document).mouseup(function (e) {
+	var popup1 = $('#consultation');
+		if (e.target!=popup1[0]&&popup1.has(e.target).length === 0){
+			$('.popup-wrapper-consultation').removeClass("shown");}
+	});
 
 $(document).ready(function() {
 			$('#cf_field_3,select').styler();
